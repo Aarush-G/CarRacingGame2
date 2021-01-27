935b10dbb9220371971e7a34b33d0db5c7b89c12
Aarush-G <aarushg010@gmail.com>
2021-01-27 18:54:23 +0530

    Car Image has been added

--- sketch.js
+++ sketch.js
@@ -10,6 +10,22 @@ var form, player, game;
 
 var cars, car1, car2, car3, car4;
 
+var car1img, car2img, car3img, car4img;
+
+var track, ground;
+
+function preload(){
+
+  car1img = loadImage("images/car1.png")
+  car2img = loadImage("images/car2.png")
+  car3img = loadImage("images/car3.png")
+  car4img = loadImage("images/car4.png")
+
+  track = loadImage("images/track.jpg")
+  ground = loadImage("images/ground.png")
+
+}
+
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +43,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end()
   }
 }
